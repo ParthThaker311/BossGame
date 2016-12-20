f4a77fde8da8bd25e6d1fa55afe758b53903d5cb
k0ofdeath <parth.thaker.311@gmail.com>
2016-12-20 10:43:58 -0500

    Added initial turn based controls
    added isTurn, hasMoved, and numActions

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 public class PlayerController : MonoBehaviour {
 	Vector3 targetPos;
-	bool canAct;
+	bool hasMoved;
 	AStar pathfind;
 	CatmullRom rom;
+	bool isTurn;
+	int numActions = 2;
 	// Use this for initialization
 	void Start () {
 		rom = this.gameObject.GetComponent<CatmullRom> ();
@@ -17,19 +19,33 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (canAct && Input.GetMouseButtonDown (0) && TileMousePos.IsValid ()) {
-			canAct = false;
-			targetPos = new Vector3(TileMousePos.mousePos.x,this.transform.position.y,TileMousePos.mousePos.z);
-			List<Vector3> path = pathfind.FindPath (this.transform.position, targetPos);
-			rom.SetPoints (path);
-			rom.move = true;
+		if(isTurn){
+			if (!hasMoved && numActions > 0 && Input.GetMouseButtonDown (0) && TileMousePos.IsValid ()) {
+				hasMoved = true;
+				numActions--;
+				targetPos = new Vector3(TileMousePos.mousePos.x,this.transform.position.y,TileMousePos.mousePos.z);
+				List<Vector3> path = pathfind.FindPath (this.transform.position, targetPos);
+				rom.SetPoints (path);
+				rom.move = true;
+			}
+			if (this.transform.position == targetPos) {
+				rom.move = false;
+				pathfind.Reset ();
+			}
+			if(numActions <=0){
+				EndTurn();
+			}
 		}
-		if (this.transform.position != targetPos) {
-
-		} else {
-			rom.move = false;
-			canAct = true;
-			pathfind.Reset ();
-		}
+	}
+	void BeginTurn(){
+		isTurn = true;	
+		numActions = 2;
+		hasMoved = false;
+	}
+	void EndTurn(){
+		isTurn = false;	
+	}
+	public bool IsTurn(){
+		return isTurn;	
 	}
 }
