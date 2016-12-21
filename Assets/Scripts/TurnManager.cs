494329c78bfbd6b7d0b94afc348e42985c0e2d74
k0ofdeath <parth.thaker.311@gmail.com>
2016-12-21 15:10:59 -0500

    added heroes to TurnManager

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -4,30 +4,44 @@ using System.Collections;
 using System.Collections.Generic;
 public class TurnManager : MonoBehaviour {
 	List<Player> playerList;
+	List<Hero> heroList;
 	int turn;
-	bool turnStarted;
+	bool playerTurnStarted;
+	bool heroTurnStarted;
 	void Start(){
 		playerList = PlayerList.GetPlayerList();
+		heroList = HeroList.GetHeroList();
 		turn = 1;
 		Debug.Log ("Starting Turn: " + turn);
 	}
 	void Update(){
-		if (turn % 2 != 0 && !turnStarted) {
-			turnStarted = true;
+		if (turn % 2 != 0 && !playerTurnStarted) {
+			EndHeroTurn();
+			playerTurnStarted = true;
 			foreach (Player obj in playerList) {
 				obj.BeginTurn ();
 			}
-		} else if(turn % 2 == 0){
-			EndTurn ();
+		} else if(turn % 2 == 0 && !heroTurnStarted){
+			EndPlayerTurn ();
+			heroTurnStarted = true;
+			foreach(Hero obj in heroList){
+				obj.BeginTurn();
+			}
 		}
 		if(Input.GetKeyDown (KeyCode.T)) {
 			turn++;
 			Debug.Log ("Starting Turn: " + turn);
 		}
 	}
-	void EndTurn(){
-		turnStarted = false;
+	void EndPlayerTurn(){
+		playerTurnStarted = false;
 		foreach(Player obj in playerList){
+			obj.EndTurn();
+		}
+	}
+	void EndHeroTurn(){
+		heroTurnStarted = false;
+		foreach(Hero obj in heroList){
 			obj.EndTurn();
 		}
 	}
