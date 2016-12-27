1f1b4bb200f3a1deb42faa2ed92e0b64cb3069e9
k0ofdeath <parth.thaker.311@gmail.com>
2016-12-27 12:08:02 -0500

    Updated to use Level.floor
    Removed floor 2d array and moved to static Level class

--- Assets/Scripts/Tiles/TileManager.cs
+++ Assets/Scripts/Tiles/TileManager.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class TileManager : MonoBehaviour {
 	int levelLength;
 	int levelWidth;
-	private int[,] floor;
 	public GameObject tile;
 	public GameObject wall;
 	int tileSize;
@@ -13,7 +12,6 @@ public class TileManager : MonoBehaviour {
 		levelLength = Level.length;
 		levelWidth = Level.width;
 		tileSize = Level.tileSize;
-		floor = new int[levelWidth, levelLength];
 		GenerateTiles ();
 		CreateTiles ();
 	}
@@ -26,23 +24,23 @@ public class TileManager : MonoBehaviour {
 		for (int i = 0; i < levelWidth; i++) {
 			for (int j = 0; j < levelLength; j++) {
 				if (i == 0 || j == 0 || i == 9 || j == 9) {
-					floor [i, j] = 1;
+					Level.floor [i, j] = 1;
 				} else {
-					floor [i, j] = 0;
+					Level.floor [i, j] = 0;
 				}
 			}
 		}
-		floor [2, 8] = 1;
-		floor [3, 3] = 1;
+		Level.floor [2, 8] = 1;
+		Level.floor [3, 3] = 1;
 	}
 	void CreateTiles(){
 		for (int i = 0; i < levelWidth; i++) {
 			for (int j = 0; j < levelLength; j++) {
-				if (floor [i, j] == 0) {
+				if (Level.floor [i, j] == 0) {
 					GameObject obj = Instantiate (tile, new Vector3 (i * tileSize, .25f, j * tileSize), tile.transform.rotation) as GameObject;
 					obj.transform.parent = this.gameObject.transform;
 					obj.transform.name = "floor" + i.ToString () + j.ToString ();
-				} else if (floor [i, j] == 1) {
+				} else if (Level.floor [i, j] == 1) {
 					GameObject obj = Instantiate (wall, new Vector3 (i * tileSize, .25f, j * tileSize), tile.transform.rotation) as GameObject;
 					obj.transform.parent = this.gameObject.transform;
 					obj.transform.name = "wall" + i.ToString () + j.ToString ();
